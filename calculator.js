b6a445da1e6e508181b24f5dc7c72a2b719a60d1
Jessica Moore <jessica.cmoore1@gmail.com>
2019-07-02 16:23:18 -0400

    Complete

--- calculator.js
+++ calculator.js
@@ -1,10 +1,13 @@
-function add() {
+function add(int1, int2) {
+  return int1 + int2;
 }
 
-function subtract() {
+function subtract(int1, int2) {
+  return int1 - int2;
 }
 
-function sum() {
+function sum(arr) {
+  return arr.reduce((acc, curr) => acc + curr, 0);
 }
 
 module.exports = {add, subtract, sum}
